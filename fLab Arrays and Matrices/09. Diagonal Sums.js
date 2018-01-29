b08369e732436e66dd14921486af2b7c4f3ca9a3
hp <teddybear90@abv.bg>
2018-01-30 01:20:13 +0200

    Lab

---
+++ fLab Arrays and Matrices/09. Diagonal Sums.js
@@ -0,0 +1,13 @@
+function diagonalSums(input) {
+    let matrix = input.map(rows => rows. map(Number));
+
+    let main = 0, secondary = 0;
+
+    for(let row = 0; row < matrix.length; row++) {
+        main += matrix[row][row];
+        secondary += matrix[row][matrix[row].length - row - 1];
+    }
+
+    console.log(`${main} ${secondary}`);
+}
+diagonalSums([[20, 40], [10, 60]]);
